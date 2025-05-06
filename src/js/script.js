b811c51fdf4daed4969e82955a2a663c83cc20ca
matin <matinehelahi@gmail.com>
2025-05-06 10:43:28 +0330

    update project

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,26 @@ if (navClose){
     });
 }
 /*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== API ===============*/
+
+async function fetchVTGPrice() {
+    try {
+      const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=victory-gem&vs_currencies=usd');
+      const data = await res.json();
+      const price = data['victory-gem'].usd;
+  
+      document.getElementById('vtg-price').innerText = price.toFixed(6); // نمایش با 6 رقم اعشار
+    } catch (error) {
+      document.getElementById('vtg-price').innerText = "خطا در دریافت قیمت";
+      console.error(error);
+    }
+  }
+  
+  // اولین بار اجرا
+  fetchVTGPrice();
+  
+  // هر 60 ثانیه یک‌بار آپدیت
+  setInterval(fetchVTGPrice, 60000);
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,11 @@ module.exports = {
       'asilver': '#444',
       'backgroundsilver': 'rgba(191, 191, 191, .09);',
       'violet': '#ddd6fe',
+      'violets': '#ede9fe',
+      'violetss': '#f5f3ff',
       'dark-hover-off': '#5d43db;',
       'buttonHome': '#007aff;',
+      'red': '#b91c1c',
 
     },
     extend: {},
